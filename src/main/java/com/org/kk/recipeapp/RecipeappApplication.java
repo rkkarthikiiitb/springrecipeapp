58009cac8c372ed1c2916412b336c8eace4a8365
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-01-29 16:54:51 +1100

    Added model classes.

---
+++ src/main/java/com/org/kk/recipeapp/RecipeappApplication.java
@@ -0,0 +1,14 @@
+package com.org.kk.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeappApplication.class, args);
+	}
+
+}
+
