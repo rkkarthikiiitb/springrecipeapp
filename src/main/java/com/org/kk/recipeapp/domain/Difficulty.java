1ec8dc1ef67f689327b0e3f750fefed55eaa6a6e
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-01-30 10:14:58 +1100

    Added required enum for difficulty settings

---
+++ src/main/java/com/org/kk/recipeapp/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package com.org.kk.recipeapp.domain;
+
+public enum Difficulty {
+	
+	EASY, MODERATE, HARD
+
+}
--- src/main/java/com/org/kk/recipeapp/domain/Recipe.java
+++ src/main/java/com/org/kk/recipeapp/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,6 +33,9 @@ public class Recipe {
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
+	
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
 
 	public Long getId() {
 		return id;
@@ -118,6 +123,14 @@ public class Recipe {
 
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
 	}
 
 }
