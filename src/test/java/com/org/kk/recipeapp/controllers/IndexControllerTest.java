c835d338746c14512655c01bd5b7a219dbecae6e
Karthik Kunjithapatham <karthikk@Karthiks-MacBook-Pro.local>
2019-02-04 13:05:12 +1100

    Added argument captor for tests

--- src/test/java/com/org/kk/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/org/kk/recipeapp/controllers/IndexControllerTest.java
@@ -1,16 +1,22 @@
 package com.org.kk.recipeapp.controllers;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
-import static org.mockito.Mockito.*;
 
+import com.org.kk.recipeapp.domain.Recipe;
 import com.org.kk.recipeapp.services.RecipeServiceImpl;
 
 public class IndexControllerTest {
@@ -31,11 +37,17 @@ public class IndexControllerTest {
 
 	@Test
 	public void testGetIndex() {
-		
+		Set<Recipe> recipes = new HashSet<>();
+		recipes.add(new Recipe());
+		recipes.add(new Recipe());
+		Mockito.when(recipeService.getRecipes()).thenReturn(recipes);
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 		String viewName = indexController.getIndex(model);
 		assertEquals("index", viewName);
 		Mockito.verify(recipeService, times(1)).getRecipes();
-		Mockito.verify(model, times(1)).addAttribute(eq("recipes"),anySet());
+		Mockito.verify(model, times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+		Set<Recipe> setInController = argumentCaptor.getValue();
+		assertEquals(2, setInController.size());
 	}
 
 }
